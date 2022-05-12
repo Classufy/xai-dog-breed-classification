1290551d16f179b1d97d275c93bae2632daa4b9c
Mingyu Park <alsrb4298@naver.com>
2022-05-13 03:02:02 +0900

    [Update] 모델 전이학습으로 변경

--- src/model.py
+++ src/model.py
@@ -10,8 +10,8 @@ import pandas as pd
 data_path = './data'
 
 target = [
-    'beagle', 'cocker_spaniel',
-    'maltese', 'pomeranian', 'poodle',
+    'beagle', 'cocker_spaniel', 'golden_retriever',
+    'maltese', 'pekinese', 'pomeranian', 'poodle',
     'samoyed', 'shih_tzu', 'white_terrier']
 
 image_dir = [f'{data_path}/{breed}' for breed in target]
@@ -24,10 +24,11 @@ for i, breed in enumerate(target):
     jpg_list = glob.glob(f'{data_path}/{breed}/*.jpg')
     for img in jpg_list:
         img = Image.open(img)
-        img = img.resize((128, 128))
+        img = img.resize((224, 224))
         img = img.convert('RGB')
         X.append(np.array(img))
         y.append(i) 
+    print(f'{breed} * {len(jpg_list)} images are converted')
 
 X = np.array(X)
 y = np.array(y)
@@ -52,34 +53,50 @@ y_train.shape
 X_train, X_val, y_train, y_val = train_test_split(
     X_train, y_train, random_state=1, test_size=0.2)
 
-model = tf.keras.Sequential([
-  tf.keras.layers.Conv2D((128), (3, 3), activation='relu', input_shape=(128, 128, 3)),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D((128), (3, 3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Dense(len(target), activation='softmax')
-])
+base_model = tf.keras.applications.xception.Xception(
+    weights='imagenet', include_top=False)
 
-# optimizer = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, bete_2=0.999)
+avg = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+output = tf.keras.layers.Dense(len(target), activation='softmax')(avg)
+model = tf.keras.Model(inputs=base_model.input, outputs=output)
+
+# 가중치들 다 고정하고 전이 학습
+for layer in base_model.layers:
+    layer.trainable = False
+
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.2, momentum=0.9, decay=0.01)
+early_stopping_cb = tf.keras.callbacks.EarlyStopping(
+    patience=2, restore_best_weights=True)
 
 model.compile(
-    optimizer='adam',
+    optimizer=optimizer,
     loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['accuracy'])
 
-early_stopping_cb = tf.keras.callbacks.EarlyStopping(
-    patience=5, restore_best_weights=True)
 
 history = model.fit(
-    X_train, y_train, epochs=100, 
+    X_train, y_train, epochs=5,
     validation_data=(X_val, y_val),
     callbacks=[early_stopping_cb])
 
-model.evaluate(X_test, y_test)
+# 마지막 5 레이어의 가중치만 열고 다시 학습
+for layer in base_model.layers[-5:-1]:
+    layer.trainable = True
+
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, decay=0.001)
+
+model.compile(
+    optimizer=optimizer,
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+history = model.fit(
+    X_train, y_train, epochs=100,
+    validation_data=(X_val, y_val),
+    callbacks=[early_stopping_cb])
+
+print('test loss / accuracy')
+print(model.evaluate(X_test, y_test))
 
 pd.DataFrame(history.history).plot(figsize=(8, 5))
 plt.grid(True)
